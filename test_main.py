bbc7efc4cc8a33e7b46d6db81a6fac5a30613211
Nakiyah24 <Nakiyah24>
2024-11-17 20:12:36 +0000

    again

--- test_main.py
+++ test_main.py
@@ -14,20 +14,20 @@ FILESTORE_PATH = "dbfs:/FileStore/nd191_assignment11"
 url = f"https://{server_h}/api/2.0"
 
 
-def check_filestore_path(path, headers): 
+def check_filestore_path(url2, path, headers): 
     try:
-        response = requests.get(url + f"/dbfs/get-status?path={path}", headers=headers)
+        response = requests.get(url2 + f"/dbfs/get-status?path={path}", headers=headers)
         response.raise_for_status()
         return response.json().get('path') is not None
     except requests.exceptions.RequestException as e:
         print(f"Request error: {e}")
         return False
 
-def test_databricks():
+def test_databricks(url1):
     headers = {'Authorization': f'Bearer {access_token}'}
-    assert check_filestore_path(FILESTORE_PATH, headers) is True, "Filestore path does not exist or is inaccessible."
+    assert check_filestore_path(url1, FILESTORE_PATH, headers) is True, "Filestore path does not exist or is inaccessible."
 
 if __name__ == "__main__":
     print("Testing Databricks")
-    test_databricks()
+    test_databricks(url)
     print("Test worked")
