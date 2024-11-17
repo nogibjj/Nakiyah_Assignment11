06bd6c7b36d1cab6f9140488687fe3c9eda4e84f
Nakiyah24 <Nakiyah24>
2024-11-17 19:55:23 +0000

    checking

--- test_main.py
+++ test_main.py
@@ -1,10 +1,10 @@
 """
 Test databricks fucntionaility
 """
+# test_main.py
 import requests
 from dotenv import load_dotenv
 import os
-
 
 # Load environment variables
 load_dotenv()
@@ -13,23 +13,18 @@ access_token = os.getenv("ACCESS_TOKEN")
 FILESTORE_PATH = "dbfs:/FileStore/mini_project11"
 url = f"https://{server_h}/api/2.0"
 
-print(f"Server Hostname: {server_h}")
-
-
-# Function to check if a file path exists and auth settings still work
 def check_filestore_path(path, headers): 
     try:
         response = requests.get(url + f"/dbfs/get-status?path={path}", headers=headers)
         response.raise_for_status()
-        return response.json()['path'] is not None
-    except Exception as e:
-        print(f"Error checking file path: {e}")
+        return response.json().get('path') is not None
+    except requests.exceptions.RequestException as e:
+        print(f"Request error: {e}")
         return False
 
-# Test if the specified FILESTORE_PATH exists
 def test_databricks():
     headers = {'Authorization': f'Bearer {access_token}'}
-    assert check_filestore_path(FILESTORE_PATH, headers) is True
+    assert check_filestore_path(FILESTORE_PATH, headers) is True, "Filestore path does not exist or is inaccessible."
 
 if __name__ == "__main__":
     print("Testing Databricks")
